caef35e37a82d2f2175d1d01180cc4d50adbd164
Acer <konskiidima@gmail.com>
2026-05-13 16:55:28 +0200

    first commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+from unittest.mock import patch, MagicMock
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_crypto_buy_more(mock_prediction: MagicMock) -> None:
+    current_rate = 100
+
+    mock_prediction.return_value = 106
+
+    result = cryptocurrency_action(current_rate)
+
+    assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_crypto_sell_all(mock_prediction: MagicMock) -> None:
+    current_rate = 100
+
+    mock_prediction.return_value = 94
+
+    result = cryptocurrency_action(current_rate)
+
+    assert result == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_crypto_do_nothing(mock_prediction: MagicMock) -> None:
+    current_rate = 100
+
+    mock_prediction.return_value = 101
+
+    result = cryptocurrency_action(current_rate)
+
+    assert result == "Do nothing"
